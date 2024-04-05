924be48f553cf64f7648de5629e64fee103d2dc7
cyrilladrian <cyrilladrian.wicaksono@gmail.com>
2024-04-05 16:16:19 +0700

    [ADD] add setter function and validate rating input

--- src/main/java/id/ac/ui/cs/advprog/gametime/model/Review.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/model/Review.java
@@ -3,12 +3,13 @@ package id.ac.ui.cs.advprog.gametime.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
 
-@Getter @Setter
+@Getter
 public class Review {
     private UUID reviewId;
     private String reviewTitle;
@@ -17,8 +18,21 @@ public class Review {
     private String reviewText;
 
     private UUID userID;
-    private UUID id;
+    private UUID gameID;
 
+
+    public Review(UUID reviewId, String title, float rating, String reviewText){
+        this.reviewId = reviewId;
+        this.reviewTitle = title;
+        this.reviewText = reviewText;
+        this.sellerResponses = new HashSet<>();
+
+        if (rating <= 5.0 || rating >= 0.0){
+            this.rating = rating;
+        } else{
+            throw new IllegalArgumentException();
+        }
+    }
     public void addSellerResponse(String response){
         this.sellerResponses.add(response);
     }
