6d9a0dc2f33376e35853fcd413b32f737a743628
fiardiel <rafi.ardiel@ui.ac.id>
2024-04-22 00:30:49 +0700

    FEAT: Added GameRepository

---
+++ src/main/java/id/ac/ui/cs/advprog/gametime/repository/GameRepository.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.gametime.repository;
+
+import id.ac.ui.cs.advprog.gametime.model.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface GameRepository extends JpaRepository<Game, UUID> {
+    Game findGameById(UUID id);
+    boolean deleteGameById(UUID id);
+}
