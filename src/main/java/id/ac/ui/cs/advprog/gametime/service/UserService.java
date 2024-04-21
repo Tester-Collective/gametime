09e5e5d7de37a730bbbcab321a720e29670e96e3
Muhammad Oka <oka@muhammadoka.dev>
2024-04-22 00:05:17 +0700

    feat: add user service

--- src/main/java/id/ac/ui/cs/advprog/gametime/service/UserService.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/service/UserService.java
@@ -1,2 +1,41 @@
-package id.ac.ui.cs.advprog.gametime.service;public class UserService {
+package id.ac.ui.cs.advprog.gametime.service;
+
+import id.ac.ui.cs.advprog.gametime.model.User;
+import id.ac.ui.cs.advprog.gametime.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByUsername(username);
+        if (user.isPresent()) {
+            return org.springframework.security.core.userdetails.User.builder()
+                    .username(user.get().getUsername())
+                    .password(user.get().getPassword())
+                    .roles(getRoles(user.get()))
+                    .build();
+        } else {
+            throw new UsernameNotFoundException("User not found");
+        }
+    }
+
+    private String[] getRoles(User user) {
+        if (user.isAdmin()) {
+            return new String[]{"ADMIN"};
+        } else if (user.isSeller()) {
+            return new String[]{"SELLER"};
+        } else {
+            return new String[]{"USER"};
+        }
+    }
 }
