c5ec85a10b5950746f116866aa1b32f9eefcc745
Kiel <hezekial7778@gmail.com>
2024-04-21 23:46:16 +0700

    Connecting model Order with database

--- src/main/java/id/ac/ui/cs/advprog/gametime/model/Order.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/model/Order.java
@@ -1,23 +1,49 @@
 package id.ac.ui.cs.advprog.gametime.model;
 
 import enums.OrderStatus;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
 import java.util.UUID;
 
-@Setter
+@Entity
+@Table(name = "game_order")
 @Getter
+@Setter
 public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID orderId;
+
+    @Column(nullable = false)
     private UUID userId;
+
+    @ManyToMany
+    @JoinTable(
+            name = "order_game",
+            joinColumns = @JoinColumn(name = "order_id"),
+            inverseJoinColumns = @JoinColumn(name = "game_id")
+    )
     private List<Game> games;
+
+    @Column(nullable = false)
     private int quantity;
+
+    @Column(nullable = false)
     private int totalPrice;
+
+    @Column(nullable = false)
     private int gamePrice;
+
+    @Column(nullable = false)
     private int stock;
+
+    @Column(nullable = false)
     private boolean orderAccess;
+
+    @Column(nullable = false)
     private String status;
 
     public Order(UUID orderId, UUID userId, List<Game> games, int quantity, int gamePrice, int stock) {
@@ -43,6 +69,9 @@ public class Order {
     public Order(UUID orderId, UUID userId, List<Game> games, int quantity, int gamePrice, int stock, String status) {
         this(orderId, userId, games, quantity, gamePrice, stock);
         this.setStatus(status);
+    }
+
+    public Order() {
     }
 
     public void setStatus(String status) {
