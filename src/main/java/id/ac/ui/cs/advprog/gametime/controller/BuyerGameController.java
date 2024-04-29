19a8acbee957d5235b07e0320fc65edbbbe2c135
fiardiel <rafi.ardiel@ui.ac.id>
2024-04-29 14:32:45 +0700

    FEAT: Added add to cart skeleton for BuyerGameController

--- src/main/java/id/ac/ui/cs/advprog/gametime/controller/BuyerGameController.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/controller/BuyerGameController.java
@@ -1,11 +1,11 @@
 package id.ac.ui.cs.advprog.gametime.controller;
 
 import id.ac.ui.cs.advprog.gametime.model.Game;
-import id.ac.ui.cs.advprog.gametime.service.CategoryService;
 import id.ac.ui.cs.advprog.gametime.service.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -16,10 +16,7 @@ public class BuyerGameController {
     @Autowired
     private GameService gameService;
 
-    @Autowired
-    private CategoryService categoryService;
-
-    @RequestMapping("")
+    @GetMapping("")
     public String index(Model model) {
         List<Game> games = gameService.getAllGames();
         model.addAttribute("games", games);
