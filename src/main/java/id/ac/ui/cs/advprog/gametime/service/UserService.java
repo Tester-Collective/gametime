e8ff69aa4efec30e3422793370190379fa3f0c02
Muhammad Oka <oka@muhammadoka.dev>
2024-05-03 17:49:42 +0700

    feat: add find by username service

--- src/main/java/id/ac/ui/cs/advprog/gametime/service/UserService.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/service/UserService.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -29,6 +30,9 @@ public class UserService implements UserDetailsService {
         }
     }
 
+    public User findByUsername(String username) {
+        return userRepository.findByUsername(username).orElse(null);
+    }
     private String[] getRoles(User user) {
         if (user.isAdmin()) {
             return new String[]{"ADMIN"};
