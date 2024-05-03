6c32aa0e84a5f7d6a9e6fcc4cbbc7d1a0424677e
Muhammad Oka <oka@muhammadoka.dev>
2024-05-03 17:50:17 +0700

    feat: add register user service

--- src/main/java/id/ac/ui/cs/advprog/gametime/service/UserService.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/service/UserService.java
@@ -33,6 +33,36 @@ public class UserService implements UserDetailsService {
     public User findByUsername(String username) {
         return userRepository.findByUsername(username).orElse(null);
     }
+
+    public User registerUser(String username, String email, String password, String matchingPassword) {
+        if (username.isEmpty() || email.isEmpty() || password.isEmpty() || matchingPassword.isEmpty()) {
+            throw new IllegalArgumentException("Please fill all the form");
+        } else if (isEmailExist(email)) {
+            throw new IllegalArgumentException("Email already exist");
+        } else if (isUsernameExist(username)) {
+            throw new IllegalArgumentException("Username already exist");
+        } else {
+            User user = new User();
+
+            user.setUsername(username);
+            user.setEmail(email);
+            user.setPassword(new BCryptPasswordEncoder().encode(password));
+            user.setBalance(0);
+            user.setSeller(false);
+            user.setAdmin(false);
+
+            return userRepository.save(user);
+        }
+    }
+
+    private boolean isUsernameExist(String username) {
+        return userRepository.findByUsername(username).isPresent();
+    }
+
+    private boolean isEmailExist(String email) {
+        return userRepository.findByEmail(email).isPresent();
+    }
+
     private String[] getRoles(User user) {
         if (user.isAdmin()) {
             return new String[]{"ADMIN"};
