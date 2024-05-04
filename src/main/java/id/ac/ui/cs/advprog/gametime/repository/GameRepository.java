67d26247d43e468acdf979222825ca1006f4ebbb
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-05-05 01:27:43 +0700

    REFACTOR: removed unused imports

--- src/main/java/id/ac/ui/cs/advprog/gametime/repository/GameRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/repository/GameRepository.java
@@ -3,7 +3,6 @@ package id.ac.ui.cs.advprog.gametime.repository;
 import id.ac.ui.cs.advprog.gametime.model.Game;
 import id.ac.ui.cs.advprog.gametime.model.User;
 import jakarta.transaction.Transactional;
-import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
