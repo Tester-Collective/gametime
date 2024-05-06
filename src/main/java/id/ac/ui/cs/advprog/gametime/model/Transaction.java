5ac293702f1d313138e40dadf8860c97e8ae9fc8
rakhafahimshahab <rakha.fahim@ui.ac.id>
2024-05-06 13:41:51 +0700

    FEAT: add html for buyer transaction history

--- src/main/java/id/ac/ui/cs/advprog/gametime/model/Transaction.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/model/Transaction.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 @Entity
-@Table(name = "transaction")
+@Table(name = "transactions")
 @Setter @Getter
 public class Transaction {
     @Id
