bd78a3d790933be014d4e1cca4d169044b0aa5a0
Rafi Ardiel Erinaldi <rafi.ardiel@ui.ac.id>
2024-05-18 01:23:05 +0700

    FEAT: Implemented add review for game

--- src/main/java/id/ac/ui/cs/advprog/gametime/repository/ReviewRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/repository/ReviewRepository.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.gametime.repository;
 
+import id.ac.ui.cs.advprog.gametime.model.Game;
 import id.ac.ui.cs.advprog.gametime.model.Review;
 import id.ac.ui.cs.advprog.gametime.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -14,5 +15,6 @@ public interface ReviewRepository extends JpaRepository<Review, UUID> {
     List<Review> findReviewsByGameId(UUID gameId);
     List<Review> findReviewsByUser_UserID(UUID userId);
     List<Review> findReviewsByGameSeller(User seller);
+    Integer countByGame_Id(UUID gameId);
 
 }
