bb55f8fed69198ae200d57233919be60bdb47ecc
rakhafahimshahab <rakha.fahim@ui.ac.id>
2024-05-19 19:54:28 +0700

    FEAT:add transaction service test

--- src/main/java/id/ac/ui/cs/advprog/gametime/service/TransactionServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/service/TransactionServiceImpl.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.gametime.service;
 import id.ac.ui.cs.advprog.gametime.model.Game;
-import id.ac.ui.cs.advprog.gametime.model.GameInCart;
 import id.ac.ui.cs.advprog.gametime.model.Transaction;
 import id.ac.ui.cs.advprog.gametime.model.User;
 import id.ac.ui.cs.advprog.gametime.repository.TransactionRepository;
