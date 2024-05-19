bc06d1309eac7886d28c002bf0c5f1ffb353fe43
rauloaul <rafif.firmansyah@gmail.com>
2024-05-19 23:23:08 +0700

    FEAT: add FREE games on homepage

--- src/main/java/id/ac/ui/cs/advprog/gametime/controller/HomeController.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/controller/HomeController.java
@@ -1,12 +1,13 @@
 package id.ac.ui.cs.advprog.gametime.controller;
 
 import id.ac.ui.cs.advprog.gametime.model.Category;
+import id.ac.ui.cs.advprog.gametime.service.ReviewService;
+import org.springframework.ui.Model;
 import id.ac.ui.cs.advprog.gametime.model.Game;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import id.ac.ui.cs.advprog.gametime.service.FilterService;
 
 import java.util.List;
@@ -17,10 +18,16 @@ public class HomeController {
     @Autowired
     private FilterService filterService;
 
+    @Autowired
+    private ReviewService reviewService;
+
     @GetMapping("")
-    public String home(Model model, @Param("keyword") String keyword) {
-        List<Game> games = filterService.filterGame(keyword, null, null, 0, Integer.MAX_VALUE);
+    public String home(Model model,
+                       @RequestParam(required = false) Category category) {
+        List<Game> games;
+        games = filterService.filterGame(null, null, null, 0, 0);
         model.addAttribute("games", games);
+        model.addAttribute("reviewService", reviewService);
         return "home";
     }
 }
