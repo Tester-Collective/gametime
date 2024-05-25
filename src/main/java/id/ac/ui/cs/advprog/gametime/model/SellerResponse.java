434aab1a314aef9abcd0aaa8b1cd0134ba97de3f
cyrilladrian <cyrilladrian.wicaksono@gmail.com>
2024-05-26 00:03:20 +0700

    REFACTOR: refactor the review and add new selller response models

---
+++ src/main/java/id/ac/ui/cs/advprog/gametime/model/SellerResponse.java
@@ -0,0 +1,23 @@
+package id.ac.ui.cs.advprog.gametime.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "seller_response")
+@Getter
+@Setter
+public class SellerResponse {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID responseId;
+
+    @OneToOne
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    @Column
+    private String response;
+}
