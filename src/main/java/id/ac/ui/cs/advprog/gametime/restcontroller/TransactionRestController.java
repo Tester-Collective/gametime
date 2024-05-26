b6f17d0043c4574ba7f190b9188736a6bfe4f563
rakhafahimshahab <rakha.fahim@ui.ac.id>
2024-05-26 17:36:35 +0700

    REFACTOR:transaction service test full coverage

--- src/main/java/id/ac/ui/cs/advprog/gametime/restcontroller/TransactionRestController.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/restcontroller/TransactionRestController.java
@@ -5,8 +5,6 @@ import id.ac.ui.cs.advprog.gametime.model.User;
 import id.ac.ui.cs.advprog.gametime.service.TransactionService;
 import id.ac.ui.cs.advprog.gametime.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
