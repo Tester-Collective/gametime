43d7c4616a9b09694d15e94a7202e71ca57a6578
rauloaul <rafif.firmansyah@gmail.com>
2024-05-26 23:13:47 +0700

    REFACTOR: remove unused import and fix homepage bug

--- src/main/java/id/ac/ui/cs/advprog/gametime/restcontroller/HomeRestController.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/restcontroller/HomeRestController.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.gametime.restcontroller;
 
-import id.ac.ui.cs.advprog.gametime.model.Category;
 import id.ac.ui.cs.advprog.gametime.model.Game;
 import id.ac.ui.cs.advprog.gametime.service.FilterService;
 import lombok.RequiredArgsConstructor;
--- src/main/java/id/ac/ui/cs/advprog/gametime/service/FilterServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/service/FilterServiceImpl.java
@@ -1,6 +1,5 @@
 package id.ac.ui.cs.advprog.gametime.service;
 
-import id.ac.ui.cs.advprog.gametime.model.Category;
 import id.ac.ui.cs.advprog.gametime.model.Game;
 
 import id.ac.ui.cs.advprog.gametime.repository.FilterRepository;
