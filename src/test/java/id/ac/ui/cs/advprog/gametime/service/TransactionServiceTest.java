f5e6b12d28d8a6ce7cf2e6cdf4ee7ce7d7d4a97c
rakhafahimshahab <rakha.fahim@ui.ac.id>
2024-05-26 15:50:59 +0700

    REFACTOR:adjusted transaction test to new model

--- src/test/java/id/ac/ui/cs/advprog/gametime/service/TransactionServiceTest.java
+++ src/test/java/id/ac/ui/cs/advprog/gametime/service/TransactionServiceTest.java
@@ -4,8 +4,9 @@ import id.ac.ui.cs.advprog.gametime.model.Game;
 import id.ac.ui.cs.advprog.gametime.model.Order;
 import id.ac.ui.cs.advprog.gametime.model.Transaction;
 import id.ac.ui.cs.advprog.gametime.model.User;
-import id.ac.ui.cs.advprog.gametime.repository.TransactionRepository;
 import id.ac.ui.cs.advprog.gametime.repository.GameRepository;
+import id.ac.ui.cs.advprog.gametime.repository.TransactionRepository;
+import id.ac.ui.cs.advprog.gametime.service.TransactionServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
@@ -30,36 +31,46 @@ public class TransactionServiceTest {
     private TransactionServiceImpl transactionService;
 
     private User user;
+    private User seller;
     private Transaction transaction;
     private UUID transactionId;
+    private Game game;
+    private Order order;
 
     @BeforeEach
     void setUp() {
         MockitoAnnotations.openMocks(this);
+
         user = new User();
+        user.setBalance(100);
+        user.setUserID(UUID.randomUUID());
+        user.setEmail("test@test.com");
+        user.setPassword("test");
+        user.setUsername("test");
+
+        seller = new User();
+        seller.setBalance(100);
+        seller.setUserID(UUID.randomUUID());
+        seller.setEmail("test2@test.com");
+        seller.setPassword("test2");
+        seller.setUsername("test2");
+
+        game = new Game();
+        game.setId(UUID.randomUUID());
+        game.setSeller(seller);
+        game.setTitle("Mock Game");
+        game.setDescription("Mock Game Description");
+        game.setPrice(50);
+        game.setStock(5);
+
+        order = new Order();
+        order.setGameQuantity(Map.of(game, 2));
+        order.setOrderId(UUID.randomUUID());
+
         transactionId = UUID.randomUUID();
-        transaction = new Transaction(transactionId, user, new Order());
+        transaction = new Transaction(transactionId, user, order);
     }
 
-    @Test
-    void testCreateTransaction() {
-        when(transactionRepository.save(any(Transaction.class))).thenReturn(transaction);
-
-        Transaction createdTransaction = transactionService.create(transaction);
-
-        assertNotNull(createdTransaction);
-        assertEquals(transaction.getTransactionId(), createdTransaction.getTransactionId());
-        verify(transactionRepository, times(1)).save(transaction);
-    }
-    @Test
-    void testNegativeCreateTransaction() {
-        when(transactionRepository.save(any(Transaction.class))).thenReturn(null);
-
-        Transaction createdTransaction = transactionService.create(transaction);
-
-        assertNull(createdTransaction);
-        verify(transactionRepository, times(1)).save(transaction);
-    }
 
     @Test
     void testGetTransaction() {
@@ -71,9 +82,15 @@ public class TransactionServiceTest {
         assertEquals(transaction.getTransactionId(), foundTransaction.getTransactionId());
         verify(transactionRepository, times(1)).findById(transactionId);
     }
+
     @Test
     void testNegativeGetTransaction() {
-        when(transactionRepository.findById(transaction.getTransactionId())).thenReturn(Optional.empty());
+        when(transactionRepository.findById(transactionId)).thenReturn(Optional.empty());
+
+        Transaction foundTransaction = transactionService.get(transactionId);
+
+        assertNull(foundTransaction);
+        verify(transactionRepository, times(1)).findById(transactionId);
     }
 
     @Test
@@ -88,10 +105,10 @@ public class TransactionServiceTest {
         verify(transactionRepository, times(1)).findById(transactionId);
         verify(transactionRepository, times(1)).save(transaction);
     }
+
     @Test
-    void testNegativeUpdateStatus(){
+    void testNegativeUpdateStatus() {
         when(transactionRepository.findById(transactionId)).thenReturn(Optional.empty());
-        when(transactionRepository.save(any(Transaction.class))).thenReturn(transaction);
 
         Transaction updatedTransaction = transactionService.updateStatus(transactionId, "SUCCESS");
 
@@ -113,6 +130,7 @@ public class TransactionServiceTest {
         assertEquals(1, foundTransactions.size());
         verify(transactionRepository, times(1)).findByUser(user);
     }
+
     @Test
     void testNegativeFindAllTransactionOfUser() {
         when(transactionRepository.findByUser(user)).thenReturn(Collections.emptyList());
@@ -126,14 +144,9 @@ public class TransactionServiceTest {
 
     @Test
     void testFindAllTransactionOfSeller() {
-        User seller = new User();
-        Game game = new Game();
-        game.setSeller(seller);
         List<Game> games = Collections.singletonList(game);
         when(gameRepository.findGamesBySeller(seller)).thenReturn(games);
 
-        Order order = new Order();
-        order.setGameQuantity(Map.of(game, 1));
         transaction.setOrder(order);
         List<Transaction> transactions = Collections.singletonList(transaction);
         when(transactionRepository.findAll()).thenReturn(transactions);
@@ -146,16 +159,12 @@ public class TransactionServiceTest {
         verify(gameRepository, times(1)).findGamesBySeller(seller);
         verify(transactionRepository, times(1)).findAll();
     }
+
     @Test
     void testNegativeFindAllTransactionOfSeller() {
-        User seller = new User();
-        Game game = new Game();
-        game.setSeller(seller);
         List<Game> games = Collections.singletonList(game);
         when(gameRepository.findGamesBySeller(seller)).thenReturn(games);
 
-        Order order = new Order();
-        order.setGameQuantity(Map.of(game, 1));
         transaction.setOrder(order);
         List<Transaction> transactions = Collections.singletonList(transaction);
         when(transactionRepository.findAll()).thenReturn(transactions);
@@ -167,17 +176,12 @@ public class TransactionServiceTest {
         verify(gameRepository, times(1)).findGamesBySeller(any(User.class));
         verify(transactionRepository, times(1)).findAll();
     }
+
     @Test
     void testFindAllTransactionOfSellerWithMatchingGame() {
-        User seller = new User();
-        Game game = new Game();
-        game.setSeller(seller);
-        game.setId(UUID.randomUUID());
         List<Game> games = Collections.singletonList(game);
         when(gameRepository.findGamesBySeller(seller)).thenReturn(games);
 
-        Order order = new Order();
-        order.setGameQuantity(Map.of(game, 1));
         transaction.setOrder(order);
         List<Transaction> transactions = Collections.singletonList(transaction);
         when(transactionRepository.findAll()).thenReturn(transactions);
@@ -189,6 +193,28 @@ public class TransactionServiceTest {
         assertEquals(1, foundTransactions.size());
         verify(gameRepository, times(1)).findGamesBySeller(seller);
         verify(transactionRepository, times(1)).findAll();
+    }
+
+    @Test
+    void testPositiveHasSufficientBalance() {
+        user.setBalance(100);
+        transaction.setUser(user);
+        when(transactionRepository.findById(transactionId)).thenReturn(Optional.of(transaction));
+
+        boolean hasSufficientBalance = transactionService.hasSufficientBalance(transaction);
+
+        assertTrue(hasSufficientBalance);
+    }
+
+    @Test
+    void testNegativeHasSufficientBalance() {
+        user.setBalance(40);  // Insufficient balance
+        transaction.setUser(user);
+        when(transactionRepository.findById(transactionId)).thenReturn(Optional.of(transaction));
+
+        boolean hasSufficientBalance = transactionService.hasSufficientBalance(transaction);
+
+        assertFalse(hasSufficientBalance);
     }
 
 }
