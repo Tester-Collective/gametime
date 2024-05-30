c23aedf05d7e1cf6d8f20efc296e375ec0261866
fiardiel <rafi.ardiel@ui.ac.id>
2024-05-30 22:22:10 +0700

    REFACTOR: review subject and observer

--- src/main/java/id/ac/ui/cs/advprog/gametime/observer/ReviewSubject.java
+++ src/main/java/id/ac/ui/cs/advprog/gametime/observer/ReviewSubject.java
@@ -2,7 +2,12 @@ package id.ac.ui.cs.advprog.gametime.observer;
 
 import id.ac.ui.cs.advprog.gametime.model.Review;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public interface ReviewSubject {
+    List<ReviewObserver> observers = new ArrayList<>();
+    List<ReviewObserver> getObservers();
     void addObserver(ReviewObserver observer);
     void removeObserver(ReviewObserver observer);
     void notifyObservers(Review review);
